5b87a9042c9c26f9ea44e4c1677ce6abcc84ca20
Roman <149431633+Rokiart@users.noreply.github.com>
2024-01-04 20:59:44 +0100

    Create Zadaci3.cs

---
+++ TestProject/TestProject/TestApp1/ConsoleApp1/Zadaci3.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Zadaci3
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Napišite program koji učitava tri realna broja a, b i c. 
+        //    //Ako je broj a veći od broja b program ispisuje zbroj učitanih brojeva, 
+        //    //inače ako uvjet ne vrijedi program ispisuje "Nisu učitani ispravni brojevi!", te tu program završava. 
+        //    //Ispis je potrebno formatirati tako da se ispisuju i operandi i operatori. Broj decimalnih mjesta u ispisu nije bitan.
+        //    //Npr.ako se učitaju redom brojevi a = 2.5, b = 1.5 i c = 2 za zbroj učitanih brojeva(jer je a > b) potrebno je ispisati "2.5 + 1.5 + 2 = 6".
+
+
+        //    //Console.WriteLine("Unesite broj 1: ");
+        //    //double a = Convert.ToDouble(Console.ReadLine());
+
+        //    //Console.WriteLine("Unesite broj 2: ");
+        //    //double b = Convert.ToDouble(Console.ReadLine());
+
+        //    //Console.WriteLine("Unesite broj 3: ");
+        //    //double c = Convert.ToDouble(Console.ReadLine());
+
+        //    //if ( a > b )
+        //    //{
+        //    //    double zbroj = a + b + c;
+        //    //    Console.WriteLine(a + " + " + b + " + " + c  + " = " + zbroj);
+        //    //}
+        //    //else
+        //    //{
+        //    //    Console.WriteLine("Nisu učitani ispravni brojevi!");
+        //    //}
+        //}
+
+
+
+
+    }
+}
