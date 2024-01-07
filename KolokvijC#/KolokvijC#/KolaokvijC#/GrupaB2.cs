ad268e7d5b34fb33cf01b4f2b5b1e95a4fe4c848
Roman <149431633+Rokiart@users.noreply.github.com>
2024-01-07 15:36:00 +0100

    vjezbe zadataka

---
+++ KolokvijC#/KolokvijC#/KolaokvijC#/GrupaB2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolaokvijC_
+{
+    internal class GrupaB2
+    {
+        public static void Izvedi()
+        {
+            //Napišite program koji učitava 5 realnih brojeva u jednodimenzionalno polje, pritom je potrebno koristiti petlju po izboru.
+            //Nakon što su učitani svi elementi polja potrebno je ispisati sve elemente polja te vrijednost najvećeg elementa polja.
+            //Format ispisa mora biti kao u primjeru.
+            //Npr. ako su redom učitani brojevi
+            //5.5,
+            //7.8,
+            //6.2,
+            //4.7
+            //i 9
+            //potrebno je ispisati: "Elementi polja su: 5.5 7.8 6.2 4.7 9 Vrijednost najvećeg elementa polja iznosi: 9!" 
+
+
+        }
+    }
+}
